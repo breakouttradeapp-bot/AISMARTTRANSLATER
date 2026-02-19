ae43358eabe2a59816a45952c7eeccb3a480323b
root <root@ip-172-31-24-139.ap-southeast-1.compute.internal>
2026-02-19 12:45:02 +0000

    final

---
+++ VoiceTranslator/app/build/generated/source/buildConfig/release/com/voicetranslator/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.voicetranslator;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "com.voicetranslator";
+  public static final String BUILD_TYPE = "release";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0.0";
+  // Field from build type: release
+  public static final String ADMOB_APP_ID = "YOUR_ADMOB_APP_ID_HERE";
+  // Field from build type: release
+  public static final String RAPID_API_KEY = "2c482e59eemsh84b3ee988562f35p1da2bfjsn9d541cf8e4f8";
+}
