47e70cc104c881f7da843e9f8523d0a82c84d27d
root <root@ip-172-31-24-139.ap-southeast-1.compute.internal>
2026-02-20 11:52:53 +0000

    d

--- VoiceTranslator/app/build/generated/source/buildConfig/release/com/niv/aivoicetranslator/BuildConfig.java
+++ VoiceTranslator/app/build/generated/source/buildConfig/release/com/niv/aivoicetranslator/BuildConfig.java
@@ -7,8 +7,8 @@ public final class BuildConfig {
   public static final boolean DEBUG = false;
   public static final String APPLICATION_ID = "com.niv.aivoicetranslator";
   public static final String BUILD_TYPE = "release";
-  public static final int VERSION_CODE = 1;
-  public static final String VERSION_NAME = "1.0.0";
+  public static final int VERSION_CODE = 2;
+  public static final String VERSION_NAME = "1.0.1";
   // Field from build type: release
   public static final String ADMOB_APP_ID = "ca-app-pub-3940256099942544~3347511713";
   // Field from build type: release
